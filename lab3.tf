f18f1458fe8ebff983c0a0271904d51d89d25002
KroshneiRostyslav <rostyslav.kroshnei.23@pnu.edu.ua>
2026-05-06 21:14:59 +0300

    RE:Laboratorna robota 3

---
+++ lab3.tf
@@ -0,0 +1,53 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "az104-rg3"
+  location = "East US"
+}
+
+resource "azurerm_managed_disk" "disk1" {
+  name                 = "az104-disk1"
+  location             = azurerm_resource_group.rg.location
+  resource_group_name  = azurerm_resource_group.rg.name
+  storage_account_type = "Standard_LRS"
+  create_option        = "Empty"
+  disk_size_gb         = 32
+}
+
+resource "azurerm_managed_disk" "disk2" {
+  name                 = "az104-disk2"
+  location             = azurerm_resource_group.rg.location
+  resource_group_name  = azurerm_resource_group.rg.name
+  storage_account_type = "Standard_LRS"
+  create_option        = "Empty"
+  disk_size_gb         = 32
+}
+
+resource "azurerm_managed_disk" "disk3" {
+  name                 = "az104-disk3"
+  location             = azurerm_resource_group.rg.location
+  resource_group_name  = azurerm_resource_group.rg.name
+  storage_account_type = "Standard_LRS"
+  create_option        = "Empty"
+  disk_size_gb         = 32
+}
+
+resource "azurerm_managed_disk" "disk4" {
+  name                 = "az104-disk4"
+  location             = azurerm_resource_group.rg.location
+  resource_group_name  = azurerm_resource_group.rg.name
+  storage_account_type = "Standard_LRS"
+  create_option        = "Empty"
+  disk_size_gb         = 32
+}
+
+resource "azurerm_managed_disk" "disk5" {
+  name                 = "az104-disk5"
+  location             = azurerm_resource_group.rg.location
+  resource_group_name  = azurerm_resource_group.rg.name
+  storage_account_type = "StandardSSD_LRS"
+  create_option        = "Empty"
+  disk_size_gb         = 32
+}
